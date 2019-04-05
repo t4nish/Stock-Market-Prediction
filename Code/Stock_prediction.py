893d498e23e08d5eab4a4c373628b1b8efcd4951
t4nish <t4nish@gmail.com>
2019-04-04 20:47:59 -0400

    Update Stock_prediction.py

--- Code/Stock_prediction.py
+++ Code/Stock_prediction.py
@@ -4,8 +4,9 @@ Created on Fri Feb  8 17:32:15 2019
 
 @author: t4nis
 """
-
+from pmdarima.arima import auto_arima
 from pyramid.arima import auto_arima
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,27 +20,35 @@ plotly.tools.set_credentials_file(username='t4nish', api_key='0Qi04rOBqP4wELkOdZ
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
 
+
 df = pd.read_csv('C:/Users/t4nis/Desktop/Project/SP_Daily.csv')
 df.head()
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 
 #setting index as date
+#convert to daterange
 df['Date'] = pd.to_datetime(df.Date,format='%Y/%m/%d')
 df.index = df['Date']
+data = df.sort_index(ascending=True, axis=0)
+data.index
+freq=data.asfreq('D')
+freq['Close_fill'] = data['Close'].asfreq('D', method='ffill')
+freq=freq['Close_fill']
+df_size = len(freq)
+#freq.fillna((freq.mean()),inplace=True)
+
 
 #plot
 plt.figure(figsize=(16,8))
 plt.plot(df['Close'], label='Close Price history')
 
-
-data = df.sort_index(ascending=True, axis=0)
-df_size = len(data)
 train_size = round(df_size*90/100)
 test_size = round(df_size*10/100)
-df_model=data.Close
-df_train = data[:train_size]
-df_test = data[-test_size:]
+df_train = freq[:train_size]
+df_test = freq[-test_size:]
+training = df_train['Close']
+validation = df_test['Close']
 ￼
 
 
@@ -49,28 +58,38 @@ fig = result.plot()
 plt.plot(fig)
 
 
-training = df_train['Close']
-validation = df_test['Close']
+
 
 #ARIMA 
-model = auto_arima(df_model, start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True,d=1, D=1, trace=True,error_action='ignore',suppress_warnings=True,stepwise=True)
+model = auto_arima(freq, start_p=0, start_q=0,max_p=3, max_q=3, m=12,start_P=0, seasonal=True,d=1, D=1, trace=True,error_action='ignore',suppress_warnings=True,stepwise=True)
 print(model.aic())
-#Fit ARIMA: order=(3, 1, 3) seasonal_order=(0, 1, 1, 12); AIC=60963.421, BIC=61025.812, Fit time=132.644 seconds
-model.fit(training)
+#Fit ARIMA: order=(2, 1, 3) seasonal_order=(0, 1, 1, 12); AIC=84321.748, BIC=84380.180, Fit time=141.175 secondsmodel.fit(df_train)
 
-forecast = model.predict(n_periods=(len(validation)))
+forecast = model_fit.predict(n_periods=(len(df_test)))
 forecast = pd.DataFrame(forecast,index = df_test.index,columns=['Prediction'])
 
-rms=np.sqrt(np.mean(np.power((np.array(df_test['Close'])-np.array(forecast['Prediction'])),2)))
+rms=np.sqrt(np.mean(np.power((np.array(df_test)-np.array(forecast['Prediction'])),2)))
 rms
-plt.figure(figsize=(16,8))
-plt.plot(df_train['Close'])
-plt.plot(df_test['Close'])
+plt.figure(figsize=(14,8))
+plt.plot(df_train)
+plt.plot(df_test)
 plt.plot(forecast['Prediction'])
 
+
+model=SARIMAX(df_train,order=(2,1,3),seasonal_order=(0,1,1,12))
+model_fit=model.fit()
+print(model_fit.summary())
+forecast=model_fit.predict(start='2016-02-06 00:00:00', end='2019-02-08 00:00:00')
+pd.date_range(end='1/1/2018', periods=8) 
+plt.plot(df_train)
+plt.plot(df_test)
+plt.plot(forecast)
+
+
+
 #Exponential Smoothening
-fit1 = SimpleExpSmoothing(training).fit()
-fcast1 = fit1.forecast(len(validation))
+fit1 = SimpleExpSmoothing(df_train).fit()
+fcast1 = fit1.forecast(len(df_test))
 forecast_1 = pd.DataFrame(fcast1,index = df_test.index,columns=['Prediction'])
 
 plt.plot(df_train['Close'])
